c65fc3168f407936559e83182ceb2e16a9b9d0a5
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2025-09-20 10:16:43 -0300

    Voltei!!!

--- main.py
+++ main.py
@@ -95,48 +95,9 @@ async def calcular(ctx:commands.Context):
     else:
         await ctx.reply("Operação inválida. Tente novamente.")
 
-# @bot.command()
-# async def somar(ctx:commands.Context, num1:float, num2:float):
-#     res = num1 + num2
-#     await ctx.reply(f"A soma de {num1} com {num2} é: {num1} + {num2} = {res}")
-
-# @bot.command()
-# async def subtrair(ctx:commands.Context, num1:float, num2:float):
-#     res = num1 - num2
-#     await ctx.reply(f"A subtração de {num1} com {num2} é: {num1} - {num2} = {res}")
-
-# @bot.command()
-# async def multiplicar(ctx:commands.Context, num1:float, num2:float):
-#     res = num1 * num2
-#     await ctx.reply(f"O produto da multiplicação é: {num1} x {num2} = {res}")
-
-# @bot.command()
-# async def dividir(ctx:commands.Context, num1:float, num2:float):
-#     res = num1 / num2
-#     await ctx.reply(f"O produto da divisão é: {num1} / {num2} = {res}")
-
-# @bot.command()
-# async def potenciar(ctx:commands.Context, num1:float, num2:float):
-#     res = num1 ** num2
-#     await ctx.reply(f"O produto da potenciação é: {num1} ^ {num2} = {res}")
-
-# @bot.command()
-# async def raiz_quadrada(ctx:commands.Context, num1:float):
-#     res = sqrt(num1)
-#     await ctx.reply(f"A raiz quadrada de {num1} é: {num1} = {res}")
-
-
-
 @bot.command()
 async def repete(ctx:commands.Context, *,frase): # *, identifica a frase como um argumento inteiro
     await ctx.send(frase)
-
-
-
-
-
-
-
 
 @bot.event # criando um evento
 async def on_ready(): # quando ele estiver on
